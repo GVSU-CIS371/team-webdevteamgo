3bbcedf26302f68cbefd8e190771b32e72c35ddb
Evan-Bryant <evanbryantt@gmail.com>
2025-11-12 14:16:03 -0500

    Small refactor

--- attendnow/src/router.ts
+++ attendnow/src/router.ts
@@ -4,11 +4,7 @@ const routes: RouteRecordRaw[] = [
   { path: "/", name: "home", component: () => import("./views/HomeView.vue") },
   { path: "/dashboard", name: "dashboard", component: () => import("./views/DashboardView.vue") },
   { path: "/snapshot", name: "snapshot", component: () => import("./views/SnapshotView.vue") },
-  { 
-    path: "/course/:courseId/attendance", 
-    name: "attendance-history", 
-    component: () => import("./views/AttendanceHistoryView.vue") 
-  }
+  { path: "/course/:courseId/attendance", name: "attendance-history", component: () => import("./views/AttendanceHistoryView.vue") }
 ];
 
 export const router = createRouter({
