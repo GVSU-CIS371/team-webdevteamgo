a5d483239bae977e75b36a58e40faf8aca4eede9
synjinshanley <54914702+synjinshanley@users.noreply.github.com>
2025-12-03 12:10:41 -0500

    Feature/move course creation to dashboard (#28)
    * move create course to dashboard
    
    * update the layout of dashboard
    
    * can move to dashboard after creating a course
    
    ---------
    
    Co-authored-by: Evan-Bryant <evanbryantt@gmail.com>

--- attendnow/src/views/DashboardView.vue
+++ attendnow/src/views/DashboardView.vue
@@ -4,16 +4,22 @@ import CourseList from "../components/CourseList.vue";
 
 <template>
   <main class="dashboard">
-    <h1 class="title">Course Dashboard</h1>
+    <div class="title">
+      <h1>Course Dashboard</h1>
+      <RouterLink to="/create" class="btn">Create Course →</RouterLink>
+    </div>
     <CourseList />
   </main>
   
 </template>
 
 <style scoped>
-.dashboard { min-width: 1280px; max-width: 1480px; width: 100%; margin: 0 auto; padding: 1rem; }
-.title { margin: 1rem 0; color: #e5e7eb; font-weight: 600; }
+.dashboard { min-width: 0px; max-width: 1480px; width: 100%; margin: 0 auto }
+.title {display: flex; flex-direction: row; justify-content: space-between; margin: 1rem;}
+.title h1 { margin: 1rem 0; color: #e5e7eb; font-weight: 600; }
 @media (prefers-color-scheme: dark) {
-  .title { color: #e5e7eb; }
+  .title h1 { color: #e5e7eb; }
 }
+.btn { display:inline-block; min-width: 125px; max-height: 27px; margin-top: 1rem; margin-bottom: 1rem; color:#0b0d10; background:#8ab4ff; text-decoration:none; padding:.5rem .8rem; border-radius:8px; font-weight:600; }
+.btn:hover { filter: brightness(1.05); }
 </style>
